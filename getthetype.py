e20facea4c1aef73ceb46d6f728056e1828c24c2
Ruthvik Bandari <98545898+Ruthvik-Bandari@users.noreply.github.com>
2024-10-22 00:22:41 +0530

    Add files via upload
    Day-1

---
+++ getthetype.py
@@ -0,0 +1,6 @@
+x = 5
+y = "Ruthvik"
+z = 0.5
+print (type(x))
+print (type(y))
+print (type(z))
---
+++ properdeclaration.py
@@ -0,0 +1,33 @@
+myvar = "Ruthvik"
+my_var = "Ruthvik"
+_my_var = "Ruthvik"
+myVar = "Ruthvik"
+MYVAR = "Ruthvik"
+myvar2 = "Ruthvik"
+
+# these are the only proper ways to declare variables in python
+"""
+shall not have any libraries names as variable
+variables are case sensitive
+cannot start with numbers
+shall only start with _ or a - z or A - Z 
+can only contain 
+1) A - Z
+2) a - z
+3) 0 - 9
+4) _, ____
+"""
+"""
+there are 3 types of declaring variables
+1) camel case
+myVARIABLENAME
+in this the first letters are small and the rest are capital
+
+2) Pascal case
+MyVariableName
+here each word starts with a capital letter
+
+3) snake case
+_My_Variable_name
+each word is seperated by an _
+"""
